48b874e69f472a2ed9f0860c7cf1f2f3e7bff292
georgecristian97 <68052609+georgecristian97@users.noreply.github.com>
2021-01-10 20:55:30 +0200

    Update AppiumTest.py

--- AppiumTest.py
+++ AppiumTest.py
@@ -12,24 +12,22 @@ class test_1(unittest.TestCase):
         self.dc['platformName'] = 'Android'
         self.dc['deviceName'] = 'emulator-5554'
         self.dc['platformVersion'] = '11'
-        self.dc['app'] = "D:\\apks\\Login 1.0.apk"
+        self.dc['app'] = "/Users/x123/Documents/GitHub/Android-Test-Automation-login-app/Login 1.0.apk"
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.dc)
         self.driver.implicitly_wait(30)
 
 
 
-    def testFirstAutomationTest(self):
-        # if len(self.driver.find_elements_by_xpath(“ // *[@ text=’OK’]”)) > 0:
-        #     self.driver.find_element_by_xpath(“ // *[ @ text =’OK’]”).click();
+    def testTheUsernameAndPassword(self):
+
         xm = self.driver.find_elements_by_class_name('android.widget.EditText')
         for i in xm:
             if i.text in 'User name':
                 i.send_keys('company')
-                break
-        #self.driver.find_element_by_xpath(' // *[ @ text ="User name"][ @ class = "android.widget.EditText"]').send_keys('company')
-        # self.driver.find_element_by_xpath(' // *[ @ text ="Password"][]').send_keys('company')
-        # self.driver.find_element_by_xpath(' // *[ @ text ="Login"]').click()
-
+            if i.text in 'Password':
+                i.send_keys('company')
+        self.driver.find_element_by_id('tn.qmed.login:id/buttonClick').click()
+        self.driver.find_element_by_id('tn.qmed.login:id/declineButton').click()
     def tearDown(self):
         self.driver.quit()
 
